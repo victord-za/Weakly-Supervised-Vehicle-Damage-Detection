176d389ff64513b9c3bd3dd4d92e2c0182938402
Victor Dippenaar <victor.d@tuta.io>
2023-07-22 21:01:06 +0200

    Keep track of image locations, include additional damage categories

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -4,6 +4,9 @@ from dotenv import find_dotenv, load_dotenv
 import pandas as pd
 import requests
 import os
+import shutil
+from sklearn.model_selection import train_test_split
+import numpy as np
 
 class MakeDataset:
     def __init__(self):
@@ -17,7 +20,6 @@ class MakeDataset:
         load_dotenv(find_dotenv())
 
         # Read input data
-        print('This is the csv file:' + os.getenv('CSV_FILENAME'))
         csv_filename = os.getenv('CSV_FILENAME')
         self.data_folder = os.getenv('DATA_FOLDER')
         self.image_folder = os.getenv('IMAGE_FOLDER')
@@ -26,7 +28,8 @@ class MakeDataset:
         unavailable_images = self.image_from_csv_reader(csv_filename)
         self.clean_data(unavailable_images)
         self.one_hot_encode_damage()
-        self.save_to_csv()
+        self.save_interim_data()
+        #self.split_dataset()
 
     def get_csv(self, csv_filename):
         if isinstance(csv_filename, str):
@@ -34,7 +37,7 @@ class MakeDataset:
             csv_path = Path(csv_filename)
             if not csv_path.exists():
                 raise FileNotFoundError(f"{csv_path} does not exist")
-            self.df = pd.read_csv(csv_path)
+            self.df = pd.read_csv(csv_path, delimiter=",")
         elif isinstance(csv_filename, pd.DataFrame):
             # Use DataFrame directly
             self.df = csv_filename
@@ -45,6 +48,8 @@ class MakeDataset:
 
         # Read CSV file
         self.get_csv(csv_filename)
+        
+        self.image_locations = np.zeros((self.df.shape[0], 3))
 
         header_row = self.df.columns.tolist()
 
@@ -63,26 +68,35 @@ class MakeDataset:
         failures = []
         unavailable_indices = []
 
+        print(f"Downloading {total_images} images...")
         # Download images row by row
         for i, row in self.df.iterrows():
             image_id = row[id_index]
             image_url = row[url_index]
-            image_path = Path(self.image_folder) / f"{image_id}.jpg"
-
-            if image_path.exists():
+            image_paths = [
+                Path(self.image_folder) / f"{image_id}.jpg",
+                Path(self.data_folder) / 'interim' /  f"{image_id}.jpg",
+                Path(self.data_folder) / 'processed' / 'train' / f"{image_id}.jpg",
+                Path(self.data_folder) / 'processed' / 'test' / f"{image_id}.jpg",
+                Path(self.data_folder) / 'processed' / 'val' / f"{image_id}.jpg"
+            ]
+            self.image_locations[i, 0] = image_paths[0].exists()
+            self.image_locations[i, 1] = image_paths[1].exists()
+            self.image_locations[i, 2] = any(image_path.exists() for image_path in image_paths[2:])
+            if any(self.image_locations[i, :]): #any(image_path.exists() for image_path in image_paths):
                 success_counter += 1
-                print(f"Image {i+1} of {total_images} already exists")
             elif image_url:
                 try:
                     response = requests.get(image_url, stream=True, timeout=(20, 20))
                     response.raise_for_status()
                     # create parent directories if they don't exist
+                    image_path = image_paths[0]
                     os.makedirs(image_path.parent, exist_ok=True)
                     with open(image_path, "wb") as image_file:
                         for chunk in response.iter_content(chunk_size=1024):
                             image_file.write(chunk)
                     success_counter += 1
-                    print(f"Downloaded image {i+1} of {total_images}")
+                    self.image_locations[i, 0] = 1
                 except requests.exceptions.HTTPError:
                     print(f"Failed to download {image_id} from {image_url}")
                     failures.append(image_id)
@@ -97,17 +111,18 @@ class MakeDataset:
                 failures.append(image_id)
                 unavailable_indices.append(i)
 
-        self.available_image_percentage = success_counter / total_images * 100
-        print(f"Downloaded {self.available_image_percentage}% of images successfully")
         if failures:
-            print(f"Failed to download images with IDs: {', '.join(failures)}")
-
-        # Return the list of unavailable indices
+            print(f"\nFailed to download images with IDs: {', '.join(failures)}")
+        
+        self.available_image_percentage = success_counter / total_images * 100
+        print(f"\nDownloaded {self.available_image_percentage}% of images successfully")
+        
         return unavailable_indices
     
     def clean_data(self, unavailable_images):
         # Delete rows where images are not available
         self.df = self.df.drop(unavailable_images, axis=0).reset_index(drop=True)
+        self.image_locations = np.delete(self.image_locations, unavailable_images, axis=0)
 
         # Fill NaN values in damage_detail column with empty string
         self.df['damage_detail'].fillna('', inplace=True)
@@ -115,17 +130,66 @@ class MakeDataset:
         # Drop columns that are not needed
         self.df.drop(['img_url'], axis=1, inplace=True)
         self.df.drop(['evaluation_type'], axis=1, inplace=True)
+        
+        # Remove rows of ExteriorPhotos_Front and ExteriorPhotos_Rear. These images are taken from afar and are labeled with all the underlying damaged components.
+        mask = ~self.df['component'].isin(['ExteriorPhotos_Front', 'ExteriorPhotos_Rear'])
+        self.df = self.df[mask].reset_index(drop=True)
+        self.image_locations[mask]
+        
 
     def one_hot_encode_damage(self):
-        one_hot_encoded = self.df['damage_detail'].str.get_dummies(sep=',')
-        one_hot_encoded.columns = ['DAMAGE_' + col for col in one_hot_encoded.columns]
+        if any(self.image_locations[:, 0]):
+            # One hot encode damage detail column
+            one_hot_encoded = self.df['damage_detail'].str.get_dummies(sep=',')
+            one_hot_encoded.columns = ['DAMAGE_' + col for col in one_hot_encoded.columns]
+            one_hot_encoded = one_hot_encoded.rename(columns={"DAMAGE_BROKEN-OFF_COMPONENTS": "DAMAGE_MISSING"})
+            one_hot_encoded = one_hot_encoded.drop('DAMAGE_AS_EXPECTED', axis=1)
+            
+            # One hot encode condition and quality columns
+            one_hot_encoded['DAMAGE_CRACKS'] = ((self.df['condition'] == 'CRACKS') | 
+                                (self.df['condition'] == 'CRACKED')).astype(int)
+            one_hot_encoded['DAMAGE_CHIPS'] = (self.df['condition'] == 'CHIPS').astype(int)
+            one_hot_encoded['DAMAGE_PAINT'] = ((self.df['condition'] == 'FADED') | 
+                            (self.df['condition'] == 'NEEDS_POLISH') | 
+                            (self.df['quality'] == 'BELOW_AVERAGE')).astype(int)
+            one_hot_encoded['DAMAGE_MISSING'] = ((self.df['condition'] == 'MISSING') | 
+                                (one_hot_encoded['DAMAGE_MISSING'] == 1)).astype(int)
+            temp_damage = ((self.df['condition'] == 'DAMAGE') | 
+                                (self.df['condition'] == 'MINOR_DAMAGE') | 
+                                (self.df['condition'] == 'MAJOR_DAMAGE')).astype(int)
+            one_hot_encoded['DAMAGE_MISC'] = (temp_damage & one_hot_encoded.sum(axis=1) == 0)
 
-        self.df = pd.concat([self.df, one_hot_encoded], axis=1)
-        self.df.drop(['damage_detail'], axis=1, inplace=True)
+            self.df = pd.concat([self.df, one_hot_encoded], axis=1)
+            self.df.drop(['damage_detail', 'condition', 'quality', 'quality_detail'], axis=1, inplace=True)
+        
 
-    def save_to_csv(self):
-        output_filepath = Path(self.data_folder) / 'processed' / 'processed.csv'
-        self.df.to_csv(output_filepath, index=False)
+    def save_interim_data(self):
+        #output_folderpath = Path(self.data_folder) / 'interim'
+        #output_filepath = os.path.join(output_folderpath, 'interim.csv')
+        #self.df.to_csv(output_filepath, index=False)
+        #header_row = self.df.columns.tolist()
+        # Find the ID index as the column named "global_key"
+        #id_index = header_row.index("global_key")
+        #for i, row in self.df.iterrows():
+        #    image_id = row[id_index]
+        #    image_path = Path(self.image_folder) / f"{image_id}.jpg"
+        #    if image_path.exists():
+        #        shutil.move(image_path, output_folderpath)
+        output_folderpath = Path(self.data_folder) / 'interim'
+        output_filepath = os.path.join(output_folderpath, 'interim.csv')
+        if any(self.image_locations[:, 0]):
+            self.df.to_csv(output_filepath, index=False)
+
+            def move_image(row):
+                image_id = row["global_key"]
+                image_path = Path(self.image_folder) / f"{image_id}.jpg"
+                if image_path.exists():
+                    shutil.move(image_path, output_folderpath)
+
+            self.df.apply(move_image, axis=1)
+        else:
+            self.df = pd.read_csv(output_filepath, delimiter=',')
+                
 
 if __name__ == '__main__':
     dataset = MakeDataset()
